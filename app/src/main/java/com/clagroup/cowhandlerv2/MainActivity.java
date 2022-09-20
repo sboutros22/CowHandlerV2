a30ee1472e20a4043fdea0c4c90bf717144bc98d
loveslota <kslota21@jcu.edu>
2022-09-20 19:43:38 -0400

    Activity creation with button click functionality

--- app/src/main/java/com/clagroup/cowhandlerv2/MainActivity.java
+++ app/src/main/java/com/clagroup/cowhandlerv2/MainActivity.java
@@ -2,13 +2,28 @@ package com.clagroup.cowhandlerv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,OverviewPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
