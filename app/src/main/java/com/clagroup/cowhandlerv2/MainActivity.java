773346ca799633f730a5e7c941a82d4a91deda29
sboutros22 <sboutros22@jcu.edu>
2022-09-22 12:54:32 -0400

    Starting to impliment the backend where the user can add information

--- app/src/main/java/com/clagroup/cowhandlerv2/MainActivity.java
+++ app/src/main/java/com/clagroup/cowhandlerv2/MainActivity.java
@@ -1,15 +1,84 @@
 package com.clagroup.cowhandlerv2;
 
+import static android.app.ProgressDialog.show;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText CowId, Species, BirthDt, ParentMId, ParentDId, Descr;
+    private RadioGroup Vac1, Vac2;
+    private Button submitButton;
+    private FirebaseFirestore db;
+
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = FirebaseFirestore.getInstance();
+
+
+        submitButton.setOnClickListener((V) ) {
+
+            String cowId = CowId.getText().toString();
+            String species = Species.getText().toString();
+            String birthdaydDt = BirthDt.getText().toString();
+            String Description = Descr.getText().toString();
+            int vaccination1 = Vac1.getCheckedRadioButtonId();
+            int vaccination2 = Vac2.getCheckedRadioButtonId();
+            RadioButton vac1 = findViewById(vaccination1);
+            RadioButton vac2 = findViewById(vaccination2);
+
+            NewCow cow = new NewCow(cowId,species,birthdaydDt,Description,vac1,vac2);
+            uploadcow(cow);
+        });
+
     }
-    //testing to see if this will appear on my branch on github
+
+    public void uploadcow(User cow){
+        db.collection("Cow")
+                .add(cow)
+                .addOnSuccessListener((OnSuccessListener<? super DocumentReference>) {
+                    Toast.makeText(MainActivity.this, "Added successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, MainActivity.class));
+                    finish();
+                })
+                .addOnFailureListener((){
+                    Toast.makeText(this, "Failure", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    public void initializeViews(){
+        CowId = findViewById(R.id.CowId);
+
+
+    }
+
+
 }
---
+++ app/src/main/java/com/clagroup/cowhandlerv2/NewCow.java
@@ -0,0 +1,48 @@
+package com.clagroup.cowhandlerv2;
+
+import android.widget.RadioButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewCow extends AppCompatActivity {
+    private String CowId;
+    private String Descriptions;
+    private int herdNum;
+
+    public NewCow(String CowId, String Descriptions, int herdNum){
+
+       this.CowId = CowId;
+       this.Descriptions = Descriptions;
+       this.herdNum = herdNum;
+    }
+    public NewCow(){
+
+    }
+
+    public NewCow(String cowId, String species, String birthdaydDt, String description, RadioButton vac1, RadioButton vac2) {
+    }
+
+    public String getCowId() {
+        return CowId;
+    }
+
+    public void setCowId(String cowId) {
+        CowId = cowId;
+    }
+
+    public String getDescriptions() {
+        return Descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        Descriptions = descriptions;
+    }
+
+    public int getHerdNum() {
+        return herdNum;
+    }
+
+    public void setHerdNum(int herdNum) {
+        this.herdNum = herdNum;
+    }
+}
