e943d77aef4ffd22e04a8b80a47f7708400d00de
Ben Cassell <bcassell22@jcu.edu>
2022-09-27 12:55:34 -0400

    Created displayCow activity

--- app/src/main/java/com/clagroup/cowhandlerv2/displayCow.java
+++ app/src/main/java/com/clagroup/cowhandlerv2/displayCow.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class displayCow extends AppCompatActivity {
-
+//comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
