380971d38faea47cf14c9310a07362ab8a977176
loveslota <kslota21@jcu.edu>
2022-10-08 18:07:54 -0400

    Added Edit and Delete Cow activities; Fixed landing page functionality for respective buttons

--- app/src/main/java/com/clagroup/cowhandlerv2/MainActivity.java
+++ app/src/main/java/com/clagroup/cowhandlerv2/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 //Create button
-    Button btn;
+    Button btn, btn2,btn3,btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +17,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //Assign button to Create Entry view
         btn = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.displayEntryBtn);
+        btn3 = findViewById(R.id.editEntryBtn);
+        btn4 = findViewById(R.id.deleteEntryBtn);
+
 //Create button click event
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Link activities
                 Intent intent = new Intent(MainActivity.this,OverviewPage.class);
+                startActivity(intent);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Link activities
+                Intent intent = new Intent(MainActivity.this,EditEntry.class);
+                startActivity(intent);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Link activities
+                Intent intent = new Intent(MainActivity.this,EditEntry.class);
                 startActivity(intent);
             }
         });
