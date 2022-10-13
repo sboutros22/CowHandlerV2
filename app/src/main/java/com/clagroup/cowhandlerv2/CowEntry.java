3277ec7606aea4f07c5dc0709ab8146c798ff5ef
Joel Mathis <jmathis24@jcu.edu>
2022-10-13 19:07:34 -0400

    Cleaned login code and set login as first page.

--- app/src/main/java/com/clagroup/cowhandlerv2/CowEntry.java
+++ app/src/main/java/com/clagroup/cowhandlerv2/CowEntry.java
@@ -1,14 +1,109 @@
 package com.clagroup.cowhandlerv2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CowEntry extends AppCompatActivity {
+
+    private Button btn;
+    private FirebaseFirestore db;
+    public FirebaseUser currentUser;
+    private EditText CowId, Species, Descr, BirthDt, Mother, Father, HerdNum, Weight, Age;
+    private RadioGroup Vac1,Vac2, Gender;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cow_entry);
+
+        initializeViews();
+
+        btn = findViewById(R.id.submitButton);
+//Create button click event
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                db = FirebaseFirestore.getInstance();
+                String uid = currentUser.getUid();
+
+                String cowId = CowId.getText().toString();
+                String species = Species.getText().toString();
+                int genders = Gender.getCheckedRadioButtonId();
+                RadioButton gender = findViewById(genders);
+                String sex = gender.getText().toString();
+                String birthdayDt = BirthDt.getText().toString();
+                int weight = Integer.parseInt(Weight.getText().toString());
+                int age = Integer.parseInt(Age.getText().toString());
+                String mother = Mother.getText().toString();
+                String father = Father.getText().toString();
+                String Description = Descr.getText().toString();
+                int vaccination1 = Vac1.getCheckedRadioButtonId();
+                int vaccination2 = Vac2.getCheckedRadioButtonId();
+                RadioButton vacCheck1 = findViewById(vaccination1);
+                String vac1 = vacCheck1.getText().toString();
+                RadioButton vacCheck2 = findViewById(vaccination2);
+                String vac2 = vacCheck2.getText().toString();
+                Log.d("Submit button check", "did this work at all");
+
+                NewCow Cow = new NewCow(cowId, birthdayDt, age, species, weight, sex, Description, vac1, vac2, mother, father);
+                uploadCow(Cow);
+            }
+
+        });
+
+    }
+
+    public void uploadCow(NewCow Cow) {
+        db.collection("Test").document("Third Cow")
+                .set(Cow)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("Cow added Successfully", "DocumentSnapshot successfully written!");;
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Failed to add to database", "Error adding document", e);
+                    }
+                });
+
+    }
+    private void initializeViews() {
+        CowId = findViewById(R.id.cowId);
+        Species = findViewById(R.id.Species);
+        Descr = findViewById(R.id.Descr);
+        BirthDt = findViewById(R.id.BirthDt);
+        Mother = findViewById(R.id.Mother);
+        Father = findViewById(R.id.Father);
+        HerdNum = findViewById(R.id.HerdNum);
+        Weight = findViewById(R.id.Weight);
+        Age = findViewById(R.id.Age);
+        Vac1 = findViewById(R.id.Vac1);
+        Vac2 = findViewById(R.id.Vac2);
+        Gender = findViewById(R.id.Gender);
+        submitButton = findViewById(R.id.submitButton);
     }
 }
--- app/src/main/java/com/clagroup/cowhandlerv2/MainActivity.java
+++ app/src/main/java/com/clagroup/cowhandlerv2/MainActivity.java
@@ -25,6 +25,8 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
@@ -45,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup Vac1, Vac2;
     private Button submitButton;
     private FirebaseFirestore db;
+    public FirebaseUser currentUser;
 
     DatabaseReference databaseReference;
 
@@ -56,6 +59,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get firebase user
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
 //Assign button to Create Entry view
         btn = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.displayEntryBtn);
@@ -96,63 +103,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        db = FirebaseFirestore.getInstance();
 
-        Map<String, String> Cow = new HashMap<>();
-       // Map<String, NewCow> cowMap = new HashMap<>();
+                //Cow uploading shifted to enter cow page.
 
-/*
-// This will create the database from the inputs the user fills in
-        submitButton.setOnClickListener((V) -> {
-            String cowId = CowId.getText().toString();
-            String species = Species.getText().toString();
-            int birthdaydDt = Integer.parseInt(BirthDt.getText().toString());
-            int weight = Integer.parseInt(Weight.getText().toString());
-            int age = Integer.parseInt(Age.getText().toString());
-            String mother = Mother.getText().toString();
-            String father = Father.getText().toString();
-            String Description = Descr.getText().toString();
-            int herdNumber = Integer.parseInt(HerdNum.getText().toString());
-            int vaccination1 = Vac1.getCheckedRadioButtonId();
-            int vaccination2 = Vac2.getCheckedRadioButtonId();
-            RadioButton vac1 = findViewById(vaccination1);
-            RadioButton vac2 = findViewById(vaccination2);
-*/
-        //makes the first cow
-            Cow.put("cowId", "1011");
-            Cow.put("Species", "LongHorn");
-            Cow.put("birthDate", "10/11");
-            Cow.put("Description", "small black spots around face");
-            Cow.put("Vax1", "True");
-            Cow.put("Vax2", "False");
-            //makes a second cow
-        Cow.put("cowId", "1023");
-        Cow.put("Species", "LongHorn");
-        Cow.put("birthDate", "10/23");
-        Cow.put("Description", "small black spots around butt");
-        Cow.put("Vax1", "False");
-        Cow.put("Vax2", "False");
 
             // This is the current form of the database I just need to alter it and add the new value to make it larger
-            NewCow cow = new NewCow("11/9","Longhorn",119,"small black spots",1,0);
+            //NewCow cow = new NewCow("11/9","Longhorn",119,"small black spots",1,0);
            // uploadcow(cow);
 
-
-            db.collection("Cow")
-                    .add(Cow)
-                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Log.d("Successfully added", "DocumentSnapshot added with ID: " + documentReference.getId());
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.w("Failed to add to database", "Error adding document", e);
-                        }
-                    });
-
+/*
             db.collection("Cow")
                     .get()
                     .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -167,52 +126,9 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                     });
-   /*
-            // trying to get the database to show up on phone
-        DocumentReference documentReference = db.collection("Cow").document(String.valueOf(CowId));
-        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
-
-            }
-        });
-
-    */
-
-/*
-// This will call the second page on the java that will check to see if it created or not
-    public void uploadcow(NewCow cow){
-        db.collection("Cow")
-                .add(cow)
-                .addOnSuccessListener((OnSuccessListener) (documentReference) -> {
-                    Toast.makeText(MainActivity.this, "Added successfully", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(MainActivity.this, MainActivity.class));
-                    finish();
-                })
-                .addOnFailureListener((e) ->{
-                    Toast.makeText(this, "Failure", Toast.LENGTH_SHORT).show();
-                });
-    }
-//This will inilize the values and what they should be, essentially it makes a spot for them
-    public void initializeViews() {
-
-        CowId = findViewById(R.id.cowId);
-        Species = findViewById(R.id.Species);
-        Descr = findViewById(R.id.Descr);
-        BirthDt = findViewById(R.id.BirthDt);
-        Mother = findViewById(R.id.Mother);
-        Father = findViewById(R.id.Father);
-        HerdNum = findViewById(R.id.HerdNum);
-        Weight = findViewById(R.id.Weight);
-        Age = findViewById(R.id.Age);
-        Vac1 = findViewById(R.id.Vac1);
-        Vac2 = findViewById(R.id.Vac2);
-        Gender = findViewById(R.id.Gender);
-        submitButton = findViewById(R.id.submitButton);
+                    */
     }
 
- */
-    }
 }
 
 
