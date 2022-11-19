73907a7e988c1922a8ed4cd7bebad64e037f69b7
loveslota <kslota21@jcu.edu>
2022-11-18 19:34:30 -0500

    Added navigation drawer component and functionality

--- app/src/main/java/com/clagroup/cowhandlerv2/MainActivity.java
+++ app/src/main/java/com/clagroup/cowhandlerv2/MainActivity.java
@@ -2,7 +2,11 @@ package com.clagroup.cowhandlerv2;
 
 import static android.app.ProgressDialog.show;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.view.MenuItem;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -31,9 +35,13 @@ public class MainActivity extends AppCompatActivity {
 
     DatabaseReference databaseReference;
 
-//Create button
+//Create buttons
     Button btn, btn2,btn3,btn4;
     TextView credDisplay;
+
+    // Initialize nav drawer in action bar
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +54,17 @@ public class MainActivity extends AppCompatActivity {
 
         credDisplay = findViewById(R.id.credView);
         credDisplay.setText(user_cred);
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 //Assign button to Create Entry view
         btn = findViewById(R.id.btn1);
@@ -89,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
                 //Cow uploading shifted to enter cow page.
 
 
@@ -112,6 +132,20 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
                     */
+    }
+
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
